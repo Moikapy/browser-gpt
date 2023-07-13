4b920e2401c2fc30d52d84ac3f3594273bf01072
Moikapy <warrenckhan@gmail.com>
2023-07-13 19:13:30 -0400

    add type to project state

--- src/state/agent_state/index.ts
+++ src/state/agent_state/index.ts
@@ -1,3 +1,13 @@
+interface agent_state_type {
+  active_tab: string;
+  messages: any[];
+  user_input: string;
+  total_tokens_used: number;
+  completion_tokens_used: number;
+  prompt_tokens_used: number;
+  tokens_used: number;
+}
+
 const agent_state = {
   active_tab: '',
   messages: [],
@@ -5,7 +15,8 @@ const agent_state = {
   total_tokens_used: 0,
   completion_tokens_used: 0,
   prompt_tokens_used: 0,
-  tokens_used:0
-};
+  tokens_used: 0,
+} as agent_state_type;
 
 export default agent_state;
+export type {agent_state_type};
