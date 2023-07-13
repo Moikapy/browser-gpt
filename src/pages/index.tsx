08093f559f46b0e934bf8dfed273a4f9a36bf523
Moikapy <warrenckhan@gmail.com>
2023-07-13 18:23:46 -0400

    test-is i can commit broken code

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,24 @@
+import {useState} from 'react';
 import AgentProvider from '../components/AgentProvider';
-import Chat from '../components/chat';
+import Chat from '../components/ui/chat';
+import Settings from '../components/ui/settings';
+import Chat_Navbar from '../components/chat_navbar';
+import Container from '../components/common/Container';
 import GlobalStyle from '../components/styles/global_style';
 
-export default function Home() {
+export default function Index() {
+  const [isSettings, setIsSettings] = useState(false);
   return (
     <AgentProvider>
-      <Chat />
+      <Container $flex>
+        <Chat_Navbar
+          onClick={() => {
+            setIsSettings(!isSettings);
+          }}
+        />
+        {!isSettings ? <Chat /> : <Settings />}
+      </Container>
+<
       <GlobalStyle />
     </AgentProvider>
   );
