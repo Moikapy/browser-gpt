f478506a672a3cce4aefa5f80d2e27fd3da95ec5
Moikapy <warrenckhan@gmail.com>
2023-07-15 00:28:34 -0400

    add types to provider

--- src/components/AgentProvider.tsx
+++ src/components/AgentProvider.tsx
@@ -1,19 +1,23 @@
 import {createContext} from 'react';
 import {usePersistedReducer} from '../hooks/usePersistedReducer';
 import agent_reducer from '../reducer/agent_reducer';
-import agent_state from '../state/agent_state';
+import agent_state, {agent_state_type} from '../state/agent_state';
 
 export const AgentContext = createContext(null);
 //
-export default function AgentProvider({children}:{children:JSX.Element[]}) {
-  const [state, dispatch] = usePersistedReducer(
+export default function AgentProvider({children}: {children: JSX.Element[]}) {
+  const [state, dispatch]: any = usePersistedReducer(
     agent_reducer,
-    agent_state,
+    agent_state as agent_state_type,
     'agent_state'
   );
 
   return (
-    <AgentContext.Provider value={{state, dispatch}}>
+    <AgentContext.Provider
+      value={{
+        state,
+        dispatch,
+      }}>
       {children}
     </AgentContext.Provider>
   );
