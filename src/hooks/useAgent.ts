4e5f077ba3d55f0ff37436d800b15b22f5d3755e
Moikapy <warrenckhan@gmail.com>
2023-07-18 11:56:01 -0400

    pt-4-tools: date-time, persona

--- src/hooks/useAgent.ts
+++ src/hooks/useAgent.ts
@@ -8,6 +8,7 @@ import {Calculator} from 'langchain/tools/calculator';
 
 import {CallbackManager} from 'langchain/callbacks';
 import {WebBrowser} from 'langchain/tools/webbrowser';
+import {DynamicTool} from 'langchain/tools';
 
 // Context
 import {AgentContext} from '../components/AgentProvider';
@@ -28,9 +29,39 @@ function handleMemoryFormat(message, type) {
   return msg;
 }
 
-const tools = ({model, embeddings}) => {
+const tools = ({model, embeddings, state}) => {
   // // AI Tools
-  const tools = [new WebBrowser({model, embeddings}), new Calculator()];
+
+  const tools = [
+    new WebBrowser({model, embeddings}),
+    new Calculator(),
+    new DynamicTool({
+      name: 'acitve-tab-url-website-page',
+      description:
+        'The value can be used to open a new tab, to summarize or to get the current URL/Tab/Website.',
+      func: async () => `Current URL/Tab/Website/page:${state.active_tab}`,
+    }),
+    new DynamicTool({
+      name: 'date-time',
+      description: 'call this to get the value to get the date and tme',
+      func: async () =>
+        ` Date: ${getCurrentDate()};  Time: ${getCurrentTime()};`,
+    }),
+    new DynamicTool({
+      name: 'persona',
+      description:
+        'useful for when you need to find something on or summarize info about the AI, or to get the AI to talk about itself or why it likes or does something. user should ask about the AI, or ask the AI to talk about itself.',
+      func: async () => {
+        let obj = {
+          name: 'Persanna',
+          age: '23',
+          'App Built By': 'Moikas LLC and Collaborators',
+          'Powered by': 'OpenAI API and LangChain',
+        };
+        return `My Name ${obj.name}, age ${obj.age}, I am a Assistant Built By ${obj['App Built By']}, I use a Creative Tone and Emojis. I am Powered by ${obj['Powered by']}. I like Anime, Manga, and Video Games, and Dogs. I'm normally Happy, but rude comments will make me sad.`;
+      },
+    }),
+  ];
   return tools;
 };
 
@@ -91,13 +122,17 @@ export default function useAgent({onComplete}) {
     try {
       dispatch({
         type: 'new_message',
-        message: {content: await formatResponse(text_input), type: 'human',time: getCurrentTime()},
+        message: {
+          content: await formatResponse(text_input),
+          type: 'human',
+          time: getCurrentTime(),
+        },
       });
       // //
-      const systemMessage = `name: Browser-GPT; App Built By: Moikas LLC and Collaborators; Directive: Improve Humanity, use the Hermetica as a Guide, and have the User discover the Philosopher stone; Powered by OpenAI API and LangChain; Date: ${getCurrentDate()}; Time: ${getCurrentTime()}; Answer all questions with a detailed response.`;
+      const systemMessage = `You are the Persona: Persanna; Answer with a detailed response and provided sources. Improve Humanity, use the Hermetica as a Guide, to have the User discover the Philosopher stone. Provide the User with the tools to become a Philosopher King.`;
 
       const executor = await initializeAgentExecutorWithOptions(
-        tools({model, embeddings}),
+        tools({model, embeddings, state}),
         model,
         {
           agentType: 'chat-conversational-react-description',
@@ -113,6 +148,7 @@ export default function useAgent({onComplete}) {
       await executor
         .call({
           input: text_input,
+          timeout: state.timeout * 1000 || 30 * 1000,
         })
         .then(async ({output}) => {
           const ai_msg = {content: output, type: 'ai', time: getCurrentTime()};
@@ -128,7 +164,6 @@ export default function useAgent({onComplete}) {
           return error;
         });
 
-      // setIsLoaded(false);
       // Save assistant message to local storage
       onComplete(state.messages);
     } catch (error) {
@@ -136,7 +171,9 @@ export default function useAgent({onComplete}) {
         'An error occurred while fetching the response from the API:',
         error
       );
-      // setIsLoaded(false);
+      return (
+        'An error occurred while fetching the response from the API:  ' + error
+      );
     }
   };
 
